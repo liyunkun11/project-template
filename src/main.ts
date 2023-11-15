3ff1639e9677cc4cc2d12568f1eea9d0e9bf7342
liyunkun11 <liyunkun_11@163.com>
2023-11-15 13:25:16 +0800

    chore: 更新第三方库的引入

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { createApp } from "vue";
+import VueGridLayout from "vue-grid-layout";
 import "normalize.css";
 import "virtual:svg-icons-register";
 import "./permission";
 import "./styles/index.less";
 
 import App from "./App.vue";
-import directive from "./directives";
+import directiveRegister from "./directives";
+import VxeTable from "./plugins/vxe-table";
 import router from "./router";
-import store from "./store";
+import store from "./stores";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
-app.use(directive);
+app.use(directiveRegister);
+app.use(VxeTable);
+app.use(VueGridLayout);
 
 app.mount("#app");
