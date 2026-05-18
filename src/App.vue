02a0a851f1e4e16c52de4fa6b729c23a27579110
liyunkun <yunkun.li@tophant.com>
2026-05-18 14:22:57 +0800

    feat(app): 初始化主题模式设置

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import "dayjs/locale/zh-cn";
 dayjs.locale("zh-cn");
 
 const themeStore = useThemeStore();
+themeStore.setThemeMode(themeStore.themeMode);
 themeStore.applyThemeColors();
 </script>
 
